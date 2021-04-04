8df511f905313070a534ee8a991329be99f7123f
pinpingpong <81296774+pinpingpong@users.noreply.github.com>
2021-04-04 20:46:25 +0800

    Adding and showing list of tao names

--- app.py
+++ app.py
@@ -1,15 +1,22 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, request, render_template, redirect
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    
-    return "<h1 style='color: red;'>Yes</h1> <div>Last account was: oysterher</div>"
+past_accounts = ['oysterher','3dollarchickensoup']
 
-@app.route('/some_html')
+@app.route('/', methods=['GET','POST'])
+def hello_world():
+	latest_account = past_accounts[0]
+	if request.method == "POST":
+		username = request.form.get("username")
+		past_accounts.append(username)
+		return tmp()
+	return render_template('main_page.html', latest_account = latest_account)
+
+
+@app.route('/tao_usernames')
 def tmp():
-    past_accounts = ['alt1', 'alt2']
-    is_banned = "YES"
-    return render_template('index.html', is_banned=is_banned, past_accounts=past_accounts)
+	account_title = "List of past usernames"
+	return render_template('list_of_users.html', account_title=account_title, past_accounts=past_accounts)
+
+
