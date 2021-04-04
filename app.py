f55387e07a50aeaa4feca5924106d75fddbe4fad
pinpingpong <81296774+pinpingpong@users.noreply.github.com>
2021-04-04 21:58:51 +0800

    Fixed pages to indicate, delete and add users

--- app.py
+++ app.py
@@ -2,23 +2,35 @@ from flask import Flask, request, render_template, redirect
 
 app = Flask(__name__)
 
-past_accounts = ['oysterher','3dollarchickensoup']
+past_accounts = ['3dollarchickensoup', 'oysterher']
 
 @app.route('/', methods=['GET','POST'])
 def hello_world():
-	latest_account = past_accounts[0]
+	latest_account = past_accounts[-1]
 	if request.method == "POST":
 		username = request.form.get("username")
 		past_accounts.append(username)
-		return render_template('list_of_users.html', past_accounts=past_accounts)
+		return redirect('/tao_usernames')
 	return render_template('main_page.html', latest_account = latest_account)
 
 
 @app.route('/tao_usernames', methods=['GET','POST'])
 def tmp():
-	if request.method == "POST":
-		username = request.form.get("username_delete")
-		past_accounts.remove(username)
 	return render_template('list_of_users.html', past_accounts=past_accounts)
 
+@app.route('/latest_user', methods=['GET','POST'])
+def tmp2():
+	if request.method == "POST":
+		option = request.form['options']
+		past_accounts.append(past_accounts.pop(past_accounts.index(option)))
+		return redirect('/')
+	return render_template('latest_user.html', past_accounts=past_accounts)
+
+@app.route('/delete_user', methods=['GET','POST'])
+def tmp3():
+	if request.method == "POST":
+		username = request.form.get("options_delete")
+		past_accounts.remove(username)
+		return redirect('tao_usernames')
+	return render_template('delete_user.html', past_accounts=past_accounts)
 
