d4a8c313838348395f4ac4d8facabaf50472d9c3
pinpingpong <81296774+pinpingpong@users.noreply.github.com>
2021-04-04 22:35:10 +0800

    Renamed functions, included validation for name input

--- app.py
+++ app.py
@@ -5,21 +5,23 @@ app = Flask(__name__)
 past_accounts = ['3dollarchickensoup', 'oysterher']
 
 @app.route('/', methods=['GET','POST'])
-def hello_world():
+def main():
 	latest_account = past_accounts[-1]
 	if request.method == "POST":
 		username = request.form.get("username")
+		if (username.isspace()) | (username ==''):
+			feedback = 'Username cannot be blank or space'
+			return render_template('main_page.html', latest_account = latest_account, feedback = feedback)
 		past_accounts.append(username)
 		return redirect('/tao_usernames')
 	return render_template('main_page.html', latest_account = latest_account)
 
-
 @app.route('/tao_usernames', methods=['GET','POST'])
-def tmp():
+def list_of_users():
 	return render_template('list_of_users.html', past_accounts=past_accounts)
 
 @app.route('/latest_user', methods=['GET','POST'])
-def tmp2():
+def latest_user():
 	if request.method == "POST":
 		option = request.form['options']
 		past_accounts.append(past_accounts.pop(past_accounts.index(option)))
@@ -27,10 +29,9 @@ def tmp2():
 	return render_template('latest_user.html', past_accounts=past_accounts)
 
 @app.route('/delete_user', methods=['GET','POST'])
-def tmp3():
+def delete_user():
 	if request.method == "POST":
 		username = request.form.get("options_delete")
 		past_accounts.remove(username)
-		return redirect('tao_usernames')
+		return redirect('/tao_usernames')
 	return render_template('delete_user.html', past_accounts=past_accounts)
-
